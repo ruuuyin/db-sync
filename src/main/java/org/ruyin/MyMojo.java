0af6a31a58b2ba17406a662c0e329fcc1a9f8fda
Ralph Royeen A. Lagumen <rralagumen@unionbankph.com>
2023-06-09 17:25:41 +0800

    Add .idea/ folder to gitignore

--- src/main/java/org/ruyin/MyMojo.java
+++ src/main/java/org/ruyin/MyMojo.java
@@ -28,39 +28,6 @@ public class MyMojo
     public void execute()
         throws MojoExecutionException
     {
-        File f = outputDirectory;
 
-        if ( !f.exists() )
-        {
-            f.mkdirs();
-        }
-
-        File touch = new File( f, "touch.txt" );
-
-        FileWriter w = null;
-        try
-        {
-            w = new FileWriter( touch );
-
-            w.write( "touch.txt" );
-        }
-        catch ( IOException e )
-        {
-            throw new MojoExecutionException( "Error creating file " + touch, e );
-        }
-        finally
-        {
-            if ( w != null )
-            {
-                try
-                {
-                    w.close();
-                }
-                catch ( IOException e )
-                {
-                    // ignore
-                }
-            }
-        }
     }
 }
